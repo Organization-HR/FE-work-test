db0a4ea3c916f32d39a3b3ea4d55dd33cd4796e7
ldhldh07 <dhlim07@naver.com>
2024-09-04 13:20:26 +0900

    :lipstick: : 폼 영역 제작

--- 두현/240904_mortgage-repayment-calculator/script.js
+++ 두현/240904_mortgage-repayment-calculator/script.js
@@ -1,6 +1,25 @@
-body {
-    font-family: 'Plus Jakarta Sans', sans-serif;
-    font-size: 16px;
-    color: hsl(202, 55%, 16%); /* Slate 900 */
-    background-color: hsl(0, 0%, 100%); /* White */
+document.getElementById('clear-all').addEventListener('click', () => {
+  const form = document.getElementById('calculator-form');
+  form.reset();
+});
+
+document.querySelectorAll('.calculater_form__radio-button input').forEach(radio => {
+  radio.addEventListener('change', function () {
+    updateParentStyles(this);
+  });
+});
+
+
+function updateParentStyles(radio) {
+  const parent = radio.closest('.calculater_form__radio-button');
+
+  document.querySelectorAll('.calculater_form__radio-button').forEach(el => {
+    el.style.backgroundColor = 'hsl(0, 0%, 100%)'; 
+    el.style.borderColor = 'hsl(200, 24%, 40%)'; 
+  });
+
+  if (radio.checked) {
+    parent.style.backgroundColor = 'hsl(61, 70%, 90%)';
+    parent.style.borderColor = 'hsl(61, 70%, 52%)';
   }
+}
