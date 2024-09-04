f8ab575c970a44f84f71f62e75151469ee601738
ldhldh07 <dhlim07@naver.com>
2024-09-04 10:13:20 +0900

    :construction_worker: : setup

---
+++ 두현/240904_mortgage-repayment-calculator/script.js
@@ -0,0 +1,6 @@
+body {
+    font-family: 'Plus Jakarta Sans', sans-serif;
+    font-size: 16px;
+    color: hsl(202, 55%, 16%); /* Slate 900 */
+    background-color: hsl(0, 0%, 100%); /* White */
+  }
